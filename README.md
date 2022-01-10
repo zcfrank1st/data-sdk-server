9debd141fd7219d476479d32fe34349da2c11090
zcfrank1st <zhchaos@gmail.com>
2022-01-10 21:07:43 +0800

    data-sdk-server: add readme; modify pom; change api to get flexible json body

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ DataSdkServer
 
 > 埋点数据记录服务
 
+
+## API
+    Name：数据上报接口（日志）
+    URL: /log
+    Method: POST
+    Request Body: 标准 json body
+
+## 镜像化部署
+    通过jib方式打包镜像并提交dockerhub
+
--- src/main/java/com/chaos/datasdkserver/controller/LogFileStoreController.java
+++ src/main/java/com/chaos/datasdkserver/controller/LogFileStoreController.java
@@ -2,25 +2,32 @@ package com.chaos.datasdkserver.controller;
 
 
 import com.chaos.datasdkserver.entity.Result;
+import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @RestController
 public class LogFileStoreController {
-    Logger logger = LoggerFactory.getLogger(LogFileStoreController.class);
+    private Logger logger = LoggerFactory.getLogger(LogFileStoreController.class);
+    private Gson gson = new Gson();
 
     @PostMapping("/log")
-    public Result uploadDataToLog(@RequestBody Map<String, String> data) {
+    public Result uploadDataToLog(@RequestBody Map<String, Object> data) {
         Result result = new Result();
         try {
             StringBuilder values = new StringBuilder();
-            for(String value :data.values()){
-                values.append(value).append("\t");
+            for(Object value :data.values()){
+                if (value instanceof LinkedHashMap) {
+                    values.append(gson.toJson(value)).append("\t");
+                } else {
+                    values.append(value).append("\t");
+                }
             }
             values.deleteCharAt(values.length() - 1);
             logger.info(values.toString());
